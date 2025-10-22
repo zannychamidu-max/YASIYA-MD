0fe04339cbbbd138c4d9691a3b185fc36930413b
zannychamidu-max <zannychamidu@gmail.com>
2025-10-23 01:29:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?p9uTuA'
 };
